927c4f102c10d5ac507bbbda62031067e07db289
GirugaCode <ryan.dphu.nguyen@gmail.com>
2019-01-09 09:50:15 -0800

    Started project, Doing on-boarding page.

--- Fashion Flick/ViewController.swift
+++ Fashion Flick/ViewController.swift
@@ -9,12 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let questionImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "QuestionImage"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+        
+    }()
+    
+    let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        let attributedText = NSMutableAttributedString(string: "What is this?", attributes: [NSAttributedString.Key.font: UIFont.fontNames(forFamilyName: "") ])
+        
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.textAlignment = .center
+    
+        
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(descriptionTextView)
+        
+        setupLayout()
     }
-
-
+    
+    
+    private func setupLayout() {
+        let topImageContainerView = UIView()
+        view.addSubview(topImageContainerView)
+        
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        topImageContainerView.addSubview(questionImageView)
+        
+        
+        NSLayoutConstraint.activate([
+            
+            topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor),
+            topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6),
+            
+            
+            questionImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor),
+            questionImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor),
+            questionImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.6),
+            
+            descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor),
+            descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+            
+            
+            ])
+    }
 }
 
