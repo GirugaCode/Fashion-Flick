d75148ef244a727663cf2d7f9bea1e24ff30b658
GirugaCode <ryan.dphu.nguyen@gmail.com>
2019-01-09 23:53:56 -0800

    [update] on boarding can change orientations and [refactored] code

--- Fashion Flick/Controller/SwipingController.swift
+++ Fashion Flick/Controller/SwipingController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SwipingController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
+    
     let pages = [
         Page(imageName: "QuestionImage", headerText: "What is this?", bodyText: "Not sure what to wear today? Let us help you out! Fashion Flick is here to help you decide and inspire what to wear for the day so you do not have to struggle thinking of an outfit."),
         Page(imageName: "HowToUseImage", headerText: "How to Use", bodyText: "We’ll first give you the Date and Weather and you can plan out the style or occasion you are going for. Then you can select the type of style that fits you for the day. From there you can choose your choice of Male or Female, then we generate a suggested outfit for you!"),
@@ -54,7 +55,7 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         
     }
     
-    private lazy var pageControl: UIPageControl = {
+    lazy var pageControl: UIPageControl = {
         let pc = UIPageControl()
         pc.currentPage = 0
         pc.numberOfPages = pages.count
@@ -96,28 +97,5 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         collectionView?.register(PageCell.self, forCellWithReuseIdentifier: "cellId")
         collectionView?.isPagingEnabled = true
     }
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pages.count
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! PageCell
-        
-        let page = pages[indexPath.item]
-        cell.page = page
-
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        return CGSize(width: view.frame.width, height: view.frame.height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
 }
---
+++ Fashion Flick/Extensions/SwipingController+UICollectionView.swift
@@ -0,0 +1,36 @@
+//
+//  SwipingController+UICollectionView.swift
+//  Fashion Flick
+//
+//  Created by Ryan Nguyen on 1/9/19.
+//  Copyright © 2019 Danh Phu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+extension SwipingController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pages.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! PageCell
+        
+        let page = pages[indexPath.item]
+        cell.page = page
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+}
