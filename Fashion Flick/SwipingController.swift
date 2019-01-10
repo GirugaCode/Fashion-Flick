3127e3c7905349e4c381d07e48999a0b888e45e8
GirugaCode <ryan.dphu.nguyen@gmail.com>
2019-01-09 19:12:43 -0800

    [fix] page control for on boarding

--- Fashion Flick/SwipingController.swift
+++ Fashion Flick/SwipingController.swift
@@ -14,14 +14,67 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         Page(imageName: "QuestionImage", headerText: "What is this?", bodyText: "Not sure what to wear today? Let us help you out! Fashion Flick is here to help you decide and inspire what to wear for the day so you do not have to struggle thinking of an outfit."),
         Page(imageName: "HowToUseImage", headerText: "How to Use", bodyText: "We’ll first give you the Date and Weather and you can plan out the style or occasion you are going for. Then you can select the type of style that fits you for the day. From there you can choose your choice of Male or Female, then we generate a suggested outfit for you!"),
         Page(imageName: "DressUpImage", headerText: "Time to Dress Up!", bodyText: "FANTASTIC! You now have an outfit to work with! From here you can either draw inspiration from the fit or you can generate a new one to keep yourself inspired! Now go out and feel confident!")
-        
     ]
     
-//    let imageNames = ["QuestionImage", "HowToUseImage", "DressUpImage"]
-//    let headerStrings = ["What is this?", "How to Use", "Time to Dress Up!"]
+    private let skipButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Skip", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont(name: "Montserrat-ExtraLight", size: 21)
+        button.setTitleColor(#colorLiteral(red: 0.2117647059, green: 0.1529411765, blue: 0.1725490196, alpha: 1), for: .normal)
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Next", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont(name: "Montserrat-ExtraLight", size: 21)
+        button.setTitleColor(#colorLiteral(red: 0.2117647059, green: 0.1529411765, blue: 0.1725490196, alpha: 1), for: .normal)
+        button.addTarget(self, action: #selector(handleNext), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func handleNext() {
+        let nextIndex = pageControl.currentPage + 1
+        
+        let indexPath = IndexPath(item: nextIndex, section: 0)
+        pageControl.currentPage = nextIndex
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+    
+    private let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 3
+        pc.currentPageIndicatorTintColor = #colorLiteral(red: 0.4392156863, green: 0.4392156863, blue: 0.4392156863, alpha: 1)
+        pc.pageIndicatorTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        return pc
+    }()
+    
+    fileprivate func setupBottomControls() {
+        let bottomControlStackView = UIStackView(arrangedSubviews: [skipButton, pageControl, nextButton])
+        bottomControlStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlStackView.distribution = .fillEqually
+        
+        view.addSubview(bottomControlStackView)
+        
+        NSLayoutConstraint.activate([
+            bottomControlStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlStackView.heightAnchor.constraint(equalToConstant: 50)
+            
+            ])
+        
+    }
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupBottomControls()
         
         collectionView?.backgroundColor = #colorLiteral(red: 0.9058823529, green: 0.8352941176, blue: 0.7411764706, alpha: 1)
         collectionView?.register(PageCell.self, forCellWithReuseIdentifier: "cellId")
@@ -37,11 +90,7 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         
         let page = pages[indexPath.item]
         cell.page = page
-        
-//        let imageName = imageNames[indexPath.item]
-//        cell.questionImageView.image = UIImage(named: imageName)
-//        cell.descriptionTextView.text = headerStrings[indexPath.item]
-        
+
         return cell
     }
     
