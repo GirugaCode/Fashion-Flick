ec97161785c088d0f5befcbcfb1c10948c9d5eb4
GirugaCode <ryan.dphu.nguyen@gmail.com>
2019-01-15 11:27:22 -0800

    updating gitignore

---
+++ Fashion Flick/Extensions/Double+String+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Double+String+Extension.swift
+//  Fashion Flick
+//
+//  Created by Ryan Nguyen on 1/15/19.
+//  Copyright © 2019 Danh Phu Nguyen. All rights reserved.
+//
+
+import Foundation
--- Fashion Flick/Networking/ForecastService.swift
+++ Fashion Flick/Networking/ForecastService.swift
@@ -1,10 +1,9 @@
 //
-//  ClothingItemProfessional.swift
+//  ForecastService.swift
 //  Fashion Flick
 //
-//  Created by Ryan Nguyen on 1/11/19.
+//  Created by Ryan Nguyen on 1/15/19.
 //  Copyright © 2019 Danh Phu Nguyen. All rights reserved.
 //
 
 import Foundation
-
